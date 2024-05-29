ae0f9dbdf7909b7b514d61798097a83d7ed1bea4
Leah Elizabeth <actually.not.leah@gmail.com>
2024-05-29 19:16:25 +0600

    added word count and line count functionality

--- character-counter.js
+++ character-counter.js
@@ -2,6 +2,8 @@ const userText = document.getElementById("user-text");
 const resetBtn = document.getElementById("reset-button");
 const completeBtn = document.getElementById("complete-button");
 const charCountDisplay = document.getElementById("character-count");
+const wordCountDisplay = document.getElementById("word-count");
+const lineCountDisplay = document.getElementById("line-count");
 let charCounter = 0;
 
 userText.addEventListener("input", alterCounter);
@@ -10,6 +12,18 @@ completeBtn.addEventListener("click", disableInput);
 
 function alterCounter() {
     charCountDisplay.textContent = userText.value.length;
+    wordCountDisplay.textContent = userText.value.trim() ? countWords() : 0;
+    lineCountDisplay.textContent = userText.value.trim() ? countLines() : 0;
+}
+
+function countWords () {
+    const words = userText.value.trim().split(/\s+/)
+    return words.length
+}
+
+function countLines() {
+    const lines = userText.value.trim().split(".")
+    return lines.length
 }
 
 function resetText() {
